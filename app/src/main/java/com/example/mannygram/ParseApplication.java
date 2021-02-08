0c340439bce80683c459610ba0c79bf29acee43b
manuev2 <manny.4163@gmail.com>
2021-02-08 14:42:43 -0800

    Photo submit working

--- app/src/main/java/com/example/mannygram/ParseApplication.java
+++ app/src/main/java/com/example/mannygram/ParseApplication.java
@@ -3,13 +3,14 @@ package com.example.mannygram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-
+        ParseObject.registerSubclass(Post.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("S6kt4mr1ex2YCG1AxYGKcbtOkWh0lM1h56y7qtaL")
                 .clientKey("ktwqsvgPzVksb8uYev5QG0TDq6ZABJsLyCBmSU0J")
